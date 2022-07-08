7133becc35254ea6b95a1198a019dbbe3af46b50
Paxsem <costumizeonu@gmail.com>
2022-07-08 22:52:07 +0200

    Simulate database with JSON file

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-  $contacts = [
-    ["name" => "Pepe", "phone_number" => "12123432"],
-    ["name" => "Antonio", "phone_number" => "24454454"],
-    ["name" => "Nate", "phone_number" => "1218766752"]
-  ]
+  if(file_exists("contacts.json")) {
+    $contacts = json_decode(file_get_contents("contacts.json"),true);
+  }
+  else {
+    $contacts = [];
+  }
 ?>
 
 
@@ -60,7 +61,7 @@
             <a class="nav-link" href="/contacts-app/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="add.html">Add Contact</a>
+            <a class="nav-link" href="add.php">Add Contact</a>
           </li>
         </ul>
       </div>
@@ -70,6 +71,14 @@
   <main>
     <div class="container pt-4 p-3">
       <div class="row">
+        <?php if(count($contacts) == 0) :?>
+          <div class="col-md-4 mx-auto">
+            <div class="card card-body text-center">
+              <p>No contacts saved yet</p>
+              <a href="./add.php">Add One!</a>
+            </div>
+          </div>
+        <?php endif ?>
         <?php foreach ($contacts as $contact): ?>
           <div class="col-md-4 mb-3">
           <div class="card text-center">
